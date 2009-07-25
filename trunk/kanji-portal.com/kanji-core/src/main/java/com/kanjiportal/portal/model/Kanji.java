df9bb531e7c913ec9c937c8bb40996adf8b257b2
nicolas.radde <nicolas.radde@c82364b0-e520-0410-83ab-11274fb815ed>
2009-07-25 12:18:38 +0000

    [ADD] modify or add the license to all files and add the license mng support with the maven-license-plugin (LGPL)

--- trunk/kanji-portal.com/kanji-core/src/main/java/com/kanjiportal/portal/model/Kanji.java
+++ trunk/kanji-portal.com/kanji-core/src/main/java/com/kanjiportal/portal/model/Kanji.java
@@ -1,25 +1,21 @@
-/******************************************************************************
- * JBoss, a division of Red Hat                                               *
- * Copyright 2006, Red Hat Middleware, LLC, and individual                    *
- * contributors as indicated by the @authors tag. See the                     *
- * copyright.txt in the distribution for a full listing of                    *
- * individual contributors.                                                   *
- *                                                                            *
- * This is free software; you can redistribute it and/or modify it            *
- * under the terms of the GNU Lesser General Public License as                *
- * published by the Free Software Foundation; either version 2.1 of           *
- * the License, or (at your option) any later version.                        *
- *                                                                            *
- * This software is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
- * Lesser General Public License for more details.                            *
- *                                                                            *
- * You should have received a copy of the GNU Lesser General Public           *
- * License along with this software; if not, write to the Free                *
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA         *
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.                   *
- ******************************************************************************/
+/**
+ * This file is part of kanji-portal.com.
+ *
+ * kanji-portal.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kanji-portal.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with kanji-portal.com.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2008-2009 Nicolas Radde <nicolas@radde.org>
+ */
 package com.kanjiportal.portal.model;
 
 import org.hibernate.search.annotations.*;
--- trunk/kanji-portal.com/kanji-core/src/main/java/com/kanjiportal/portal/model/Meaning.java
+++ trunk/kanji-portal.com/kanji-core/src/main/java/com/kanjiportal/portal/model/Meaning.java
@@ -1,11 +1,21 @@
-/*****************************************
- *                                       *
- *  Mononara : The Kanji card reviewer   *
- *                                       *
- *   Distributable under LGPL license.   *
- *   See terms of license at gnu.org.    *
- *                                       *
- *****************************************/
+/**
+ * This file is part of kanji-portal.com.
+ *
+ * kanji-portal.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kanji-portal.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with kanji-portal.com.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2008-2009 Nicolas Radde <nicolas@radde.org>
+ */
 package com.kanjiportal.portal.model;
 
 import org.hibernate.search.annotations.DocumentId;
